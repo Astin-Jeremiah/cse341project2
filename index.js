ff2eb481f2c4d9aac66ab5808ab4b575cab8fa09
Astin-Jeremiah <43362521+Astin-Jeremiah@users.noreply.github.com>
2021-03-04 23:17:14 -0500

    set home

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/public/form.html'));
 
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs')
