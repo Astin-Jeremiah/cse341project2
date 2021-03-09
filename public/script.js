d2ee05aaf5556c776627aad1cf3b863981a7c095
Astin-Jeremiah <43362521+Astin-Jeremiah@users.noreply.github.com>
2021-03-09 16:28:33 -0500

    add project

--- public/script.js
+++
@@ -1,9 +0,0 @@
-$("#type").change(function() {
-  if ($(this).data('options') === undefined) {
-    $(this).data('options', $('#weight option').clone());
-  }
-  var id = $(this).val();
-  var options = $(this).data('options').filter('[class=' + id + ']');
-  $('#weight').html(options);
-  $('#weight').prop("disabled", false);
-});
